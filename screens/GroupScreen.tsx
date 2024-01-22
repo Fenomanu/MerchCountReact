d82e23a20c22aa3b8316390aa662cfbec4bca352
Fenomanu <61010890+Fenomanu@users.noreply.github.com>
2024-01-22 23:26:24 +0100

    Groups page

--- screens/GroupScreen.tsx
+++ screens/GroupScreen.tsx
@@ -6,18 +6,42 @@ export default function GroupScreen({navigation, route}) {
     const [idGroup, setGroup] = useState(route.params.id);
     // Accede a los parámetros pasados
     
+
     const containerExtra = {
         marginTop: StatusBar.currentHeight
       }
 
-    const { id } = route.params.id;
+    let content;
+
+    if (idGroup === 1) {
+        content = (
+            <View>
+                <Text>Contenido para el Grupo 1</Text>
+            </View>
+        );
+    } else if (idGroup === 2) {
+        content = (
+            <View>
+                <Text>Contenido para el Grupo 2</Text>
+            </View>
+        );
+    } else {
+        content = (
+            <View>
+                <Text>Contenido predeterminado</Text>
+            </View>
+        );
+    }
+
     return (
-        
         <View style={containerExtra}>
             <Button title="Go back" onPress={() => navigation.goBack()} />
+            <Button title='Go to Packs' onPress={() => setGroup(1)}/>
+            <Button title='Go to Stock' onPress={() => setGroup(2)}/>
+            <Button title='Go to Group' onPress={() => setGroup(3)}/>
             <Text>
                 {idGroup}
             </Text>
+            {content}
         </View>
-    );
-}
+    );}
